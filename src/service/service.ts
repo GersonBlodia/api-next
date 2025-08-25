0783ede73d56d68f5011de47ab4c19a109da9c8c
GersonBlodia <gerson.vera@upsjb.edu.pe>
2025-08-25 10:39:07 -0500

    frst commitsd_______dsdsdsdodsxdsdd

--- src/service/service.ts
+++ src/service/service.ts
@@ -3,6 +3,7 @@ export const verifyEmailWithHunter = async (email: string) => {
     `https://api.hunter.io/v2/email-verifier?email=${email}&api_key=${process.env.HUNTER_API_KEY}`
   );
   const data = await res.json();
+  console.log(data)
   return data.data?.result === 'deliverable';
 };
-
+         
